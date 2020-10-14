721e377047ae289d329536d8b02241b24a1e84b8
Ricardo Samuel Mendoza Núñez <rsmn.development@gmail.com>
2020-10-14 01:44:12 -0500

    Add env variable for base url.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import './App.css';
 
+let baseUrl;
+
+if (process.env.NODE_ENV !== 'production') {
+  baseUrl = process.env.REACT_APP_BASE_URL;
+} else {
+  baseUrl = process.env.BASE_URL;
+}
+
 const App = () => (
   <GithubState>
       <Router>
@@ -15,9 +23,9 @@ const App = () => (
           <Navbar icon='fab fa-github' title='Github Finder' />
           <div className='container'>
             <Switch>
-              <Route exact path='/' component={ Home } />
-              <Route exact path='/about' component={ About } />
-              <Route exact path='/user/:username' component={ User } />
+              <Route exact path={ `${baseUrl}` } component={ Home } />
+              <Route exact path={ `${baseUrl}about` } component={ About } />
+              <Route exact path={ `${baseUrl}user/:username` } component={ User } />
               <Route component={ NotFound } />
             </Switch>
           </div>
